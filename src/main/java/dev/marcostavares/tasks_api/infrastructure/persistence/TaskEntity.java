1c46ce076e38d4fc3c1a8e43e9912f2e5a17ebfc
Marcos Tavares <mjtdes.md@gmail.com>
2025-03-26 18:11:01 -0300

    feat: Prepare the JPA annotations inside the TaskEntity

--- src/main/java/dev/marcostavares/tasks_api/infrastructure/persistence/TaskEntity.java
+++ src/main/java/dev/marcostavares/tasks_api/infrastructure/persistence/TaskEntity.java
@@ -3,19 +3,60 @@ package dev.marcostavares.tasks_api.infrastructure.persistence;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import dev.marcostavares.tasks_api.domain.model.TaskStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Entity(name = "task")
 public class TaskEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Getter
     private UUID id;
+
+    @NotBlank(message = "The user must inform a title to the task")
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
     private String title;
+
+    @Getter
+    @Setter
     private String description;
-    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private TaskStatus status = TaskStatus.PENDING;
+
+    @Column(nullable = false, name = "due_date")
+    @Getter
     private LocalDateTime dueDate;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @Getter
     private LocalDateTime createdAt;
+
+    public TaskEntity(String name, String description, TaskStatus status, LocalDateTime dueDate) {
+        this.title = name;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
 
 }
